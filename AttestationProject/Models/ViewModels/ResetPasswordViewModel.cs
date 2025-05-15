72a409307fb9d92e3a1d6705ae0ce76c6c2653dc
Nursultan <nyrik4653@gmail.com>
2025-05-15 05:27:28 +0500

    Seed roles + admin area + ignore .vs

---
+++ AttestationProject/Models/ViewModels/ResetPasswordViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+public class ResetPasswordViewModel
+{
+    [Required] public string UserId { get; set; } = string.Empty;
+    [Required] public string Token { get; set; } = string.Empty;
+
+    [Required, StringLength(100, MinimumLength = 6)]
+    [DataType(DataType.Password)]
+    public string Password { get; set; } = string.Empty;
+
+    [Compare(nameof(Password)), DataType(DataType.Password)]
+    public string ConfirmPassword { get; set; } = string.Empty;
+}
